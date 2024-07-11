cb9f6ca269083b98f6e41939aacf2485064511bd
Kyle <onlyxool@gmail.com>
2024-07-11 14:41:24 -0400

    Add  Restful API for SOA

--- slackbot/example.py
+++ slackbot/example.py
@@ -1,22 +1,87 @@
+import _thread
 import ollama
 import logging
 import datetime
 from dotenv import load_dotenv
 from attachment import download, parse_audio
+
 import json
 import schedule
 import time
 from slack_sdk.errors import SlackApiError
+
+from flask import Flask, request, jsonify
+
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 
 from slacktoken import SLACK_BOT_TOKEN, SLACK_APP_TOKEN
 
+session = dict()
+
+# instantiate the app
+flask_app = Flask(__name__)
+flask_app.config.from_object(__name__)
+flask_app.secret_key = 'onlyxool'
+
+def conversation2txt(sesssion):
+    text = ''
+    for user in session.keys():
+        text += user
+        for sess in session[user]:
+            text += sess['content']
+        text += '\n'
+    return text
+
+@flask_app.route('/data_update', methods=['POST'])
+def update_data():
+    request.get_json()
+    return jsonify('OK')
+
+
+@flask_app.route('/vectorization', methods=['POST'])
+def vectorization():
+    request.get_json()
+    return jsonify('pong!')
+
+
+@flask_app.route('/embedding', methods=['POST'])
+def embedding():
+    req = request.get_json()
+    try:
+        ollama.embeddings(model='llama3', prompt=req['prrompt'])
+    except:
+        return jsonify('ERROR')
+    return jsonify('OK')
+
+@flask_app.route('/summary', methods=['POST'])
+def summary():
+    request.get_json()
+    text = conversation2txt(session)
+    text += 'summary the conversation and build a todo list for every member: '
+    message = [{'role': 'user', 'content': text}]
+    ollama.chat(model='llama3', messages=message)
+
+
+@flask_app.route('/jira_update', methods=['POST'])
+def jira_update():
+    request.get_json()
+    return jsonify('pong!')
+
+def run_flask():
+    flask_app.run()
 
 load_dotenv()
 
 app = App(token=SLACK_BOT_TOKEN)
 logging.basicConfig(level=logging.DEBUG)
+
+
+def check_llm():
+    for model in ollama.list()['models']:
+        if model.get('name') == 'llama3:latest':
+            return True
+    return False
 
 
 System_Prompt = \
@@ -29,7 +94,6 @@ In your role, you need to have a deep understanding of agile development, the ab
 '''
 
 
-session = dict()
 
 def get_event_userid(event):
     return event.get('event', {}).get('user')
@@ -107,7 +171,7 @@ def app_message(event, say):
 
     sender_id = event.get('user')
 
-    #user_id = event.get('user')
+#user_id = event.get('user')
 
     if 'files' in event:
         files = event.get('files')
@@ -143,7 +207,7 @@ def app_message(event, say):
     
     resp_msg = ollama.chat(model='llama3', messages= conversation_history[sender_id])
 
-    # Append the model's response to the user's history
+
 
     say(resp_msg['message']['content'])
 
@@ -180,10 +244,11 @@ def register_scheduleMessage():
 #    ack()
 #    logger.info(body)
 
-
 if __name__ == '__main__':
-    register_scheduleMessage()
-    handler = SocketModeHandler(app, SLACK_APP_TOKEN)
-    handler.start()
-
-    schedule_reminders()
+    if check_llm():
+        register_scheduleMessage()
+        handler = SocketModeHandler(app, SLACK_APP_TOKEN)
+        _thread.start_new_thread(run_flask, ())
+        handler.start()
+    else:
+        sys.exit('No Model')
